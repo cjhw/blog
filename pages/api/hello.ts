1b643e7bfe9673393b519614f73c11722f7d140d
caijianhaoa <1784581164@qq.com>
2022-08-18 16:48:52 +0800

    搭建项目环境

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,24 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { mock, Random } from 'mockjs';
 
 type Data = {
-  name: string
-}
+  name: string,
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json(
+    mock({
+      'list|1-10': [
+        {
+          name: Random.cname(),
+          birthday: Random.date('yyyy-MM-dd'),
+          address: Random.county(true),
+        },
+      ],
+    })
+  );
 }
