7f283af0bc24e657bd303b0a1be7bf87cc020912
caijianhaoa <1784581164@qq.com>
2022-08-23 13:20:52 +0800

    实现个人中心页，编辑个人资料，页脚

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,7 +1,12 @@
 import type { NextPage } from 'next';
+import styles from './index.module.scss';
 
 const Footer: NextPage = () => {
-  return <div>页脚</div>;
+  return (
+    <div className={styles.footer}>
+      <p>Next.js搭建的个人博客</p>
+    </div>
+  );
 };
 
 export default Footer;
--- pages/user/[id].tsx
+++ pages/user/[id].tsx
@@ -1,7 +1,96 @@
-import type { NextPage } from 'next';
+/* eslint-disable @next/next/link-passhref */
+import React from 'react';
+import Link from 'next/link';
+import { observer } from 'mobx-react-lite';
+import { Button, Avatar, Divider } from 'antd';
+import {
+  CodeOutlined,
+  FireOutlined,
+  FundViewOutlined,
+} from '@ant-design/icons';
+import ListItem from 'components/ListItem';
+import { getDataBaseConnection } from 'db/index';
+import { User, Article } from 'db/entity';
+import styles from './index.module.scss';
 
-const User: NextPage = () => {
-  return <div>我是个人中心页</div>;
+export async function getServerSideProps({ params }: { params: any }) {
+  const userId = params?.id;
+  const db = await getDataBaseConnection();
+  const user = await db.getRepository(User).findOne({
+    where: {
+      id: Number(userId),
+    },
+  });
+
+  const articles = await db.getRepository(Article).find({
+    where: {
+      user: {
+        id: Number(userId),
+      },
+    },
+    relations: ['user', 'tags'],
+  });
+
+  return {
+    props: {
+      userInfo: JSON.parse(JSON.stringify(user)),
+      articles: JSON.parse(JSON.stringify(articles)),
+    },
+  };
+}
+
+const UserDetail = (props: any) => {
+  const { userInfo = {}, articles = [] } = props;
+  const viewsCount = articles?.reduce(
+    (prev: any, next: any) => prev + next?.views,
+    0
+  );
+
+  return (
+    <div className={styles.userDetail}>
+      <div className={styles.left}>
+        <div className={styles.userInfo}>
+          <Avatar className={styles.avatar} src={userInfo?.avatar} size={90} />
+          <div>
+            <div className={styles.nickname}>{userInfo?.nickname}</div>
+            <div className={styles.desc}>
+              <CodeOutlined /> {userInfo?.job}
+            </div>
+            <div className={styles.desc}>
+              <FireOutlined /> {userInfo?.introduce}
+            </div>
+          </div>
+          <Link href="/user/profile">
+            <Button>编辑个人资料</Button>
+          </Link>
+        </div>
+        <Divider />
+        <div className={styles.article}>
+          {articles?.map((article: any) => (
+            <div key={article?.id}>
+              <ListItem article={article} />
+              <Divider />
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className={styles.right}>
+        <div className={styles.achievement}>
+          <div className={styles.header}>个人成就</div>
+          <div className={styles.number}>
+            <div className={styles.wrapper}>
+              <FundViewOutlined />
+              <span>共创作 {articles?.length} 篇文章</span>
+            </div>
+            <div className={styles.wrapper}>
+              <FundViewOutlined />
+              <span>文章被阅读 {viewsCount} 次</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default User;
+export default observer(UserDetail);
