c183816ea691263de4174c4a14c196614066a875
caijianhaoa <1784581164@qq.com>
2022-08-23 17:12:11 +0800

    自定义server脚本、errorBoundary、页面关键性能指标

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,47 @@ import '../styles/globals.css';
 import Layout from 'components/layout';
 import { StoreProvider } from 'store';
 import { NextPage } from 'next';
-
+import ErrorBoundary from 'components/ErrorBoundary';
 interface IProps {
   initialValue: Record<any, any>;
   Component: NextPage;
   pageProps: any;
+}
+
+export function reportWebVitals(mertic: any) {
+  if (mertic.label === 'web-vital') {
+    // 只拿web的指标
+    // console.log('mertic', mertic);
+  }
+
+  switch (mertic.name) {
+    case 'FCP':
+      console.log('FCP', mertic);
+      break;
+    case 'LCP':
+      console.log('LCP', mertic);
+      break;
+    case 'CLS':
+      console.log('CLS', mertic);
+      break;
+    case 'FID':
+      console.log('FID', mertic);
+      break;
+    case 'TTFB':
+      console.log('TTFB', mertic);
+      break;
+    default:
+      break;
+  }
+
+  const body = JSON.stringify(mertic);
+  const url = 'https://xxxx.com';
+
+  if (navigator.sendBeacon) {
+    navigator.sendBeacon(url, body);
+  } else {
+    fetch(url, { body, method: 'POST', keepalive: true })
+  }
 }
 
 function MyApp({ initialValue, Component, pageProps }: IProps) {
@@ -23,7 +59,11 @@ function MyApp({ initialValue, Component, pageProps }: IProps) {
   };
 
   return (
-    <StoreProvider initialValue={initialValue}>{renderLayout()}</StoreProvider>
+    <ErrorBoundary>
+      <StoreProvider initialValue={initialValue}>
+        {renderLayout()}
+      </StoreProvider>
+    </ErrorBoundary>
   );
 }
 
